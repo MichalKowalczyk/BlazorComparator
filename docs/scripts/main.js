793434e9d1245591acd16004203f62160f2053a6
Michał Kowalczyk <michal.p.kowalczyk@gmail.com>
2018-10-07 21:56:58 +0200

    Add hight resolution render

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,18 +1,29 @@
 ﻿
+
 var jsButton = document.getElementById("jsButton");
-var timerSpan = document.getElementById("jsTimer");
-var canvas = document.getElementById("myCanvas");
-
 jsButton.addEventListener("click", function (e) {
-
+    
+    var canvas = document.getElementById("myCanvas");
+    var timerSpan = document.getElementById("jsTimer");
     jsButton.setAttribute("disabled", "true");
     var t0 = performance.now();
-    draw();
+    draw(canvas, 6);
+    timerSpan.innerHTML = Math.round((performance.now() - t0)) + "ms";
+}, false);
+
+var jsButton = document.getElementById("jsButtonHigh");
+jsButton.addEventListener("click", function (e) {
+    
+    var canvas = document.getElementById("myCanvasHigh");
+    var timerSpan = document.getElementById("jsTimerHigh");
+    jsButton.setAttribute("disabled", "true");
+    var t0 = performance.now();
+    draw(canvas, 1);
     timerSpan.innerHTML = Math.round((performance.now() - t0)) + "ms";
 }, false);
 
 
-function draw() {
+function draw(canvas, pixelPerPoint) {
     var ctx = canvas.getContext("2d");
     var height = 400;
     var width = 400;
@@ -20,7 +31,7 @@ function draw() {
     var c = new Complex(-0.1, 0.65);
     var copy = null;
     var divider = 2;
-    var pixelSize = 2; // resolution
+    var pixelSize = pixelPerPoint; // resolution
 
     for (var h = -(height / divider); h < (height / divider); h = h + pixelSize) {
         for (var w = -(width / divider); w < (width / divider); w = w + pixelSize) {
